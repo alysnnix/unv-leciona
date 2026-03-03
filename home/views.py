1e95c8f7a8720678d925375dd17e0bdb9817da2f
aly <4ly.alcantara@gmail.com>
2026-03-03 00:31:06 +0000

    feat: revamp dashboard UI to match admin theme and add new charts

--- home/views.py
+++ home/views.py
@@ -1,23 +1,41 @@
 from django.views.generic import TemplateView
 from chartjs.views.lines import BaseLineChartView
+from home.models import Professor, Turma, Segmento
+import json
 
+class DashboardView(TemplateView):
+    template_name = 'home/line_chart.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        # Estatísticas reais do banco (se estiver vazio, retorna 0)
+        context['professores_count'] = Professor.objects.count()
+        context['turmas_count'] = Turma.objects.count()
+        context['segmentos_count'] = Segmento.objects.count()
+
+        # Dados para o Gráfico de Barras (Ocorrências por Dia)
+        context['bar_labels'] = json.dumps(["Segunda", "Terça", "Quarta", "Quinta", "Sexta"])
+        context['bar_data'] = json.dumps([12, 19, 3, 5, 2])
+        
+        # Dados para o Gráfico de Rosca (Distribuição de Turmas por Período)
+        context['pie_labels'] = json.dumps(["Manhã", "Tarde", "Integral"])
+        context['pie_data'] = json.dumps([45, 35, 20])
+        
+        return context
 
 class LineChartJSONView(BaseLineChartView):
     def get_labels(self):
-        # Rótulos no eixo X
-        return ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho"]
+        return ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho"]
 
     def get_providers(self):
-        # Títulos dos conjuntos de dados (linhas)
-        return ["Central", "Leste", "Oeste"]
+        return ["Faltas Registradas", "Substituições Realizadas"]
 
     def get_data(self):
-        # Dados de cada linha
         return [
-            [75, 44, 92, 11, 44, 95, 35],
-            [41, 92, 18, 3, 73, 87, 92],
-            [87, 21, 94, 3, 90, 13, 65]
+            [12, 8, 4, 15, 9, 5],  # Faltas
+            [10, 7, 3, 14, 8, 4]   # Substituições
         ]
-line_chart = TemplateView.as_view(template_name='home/line_chart.html')
+
+line_chart = DashboardView.as_view()
 line_chart_json = LineChartJSONView.as_view()
